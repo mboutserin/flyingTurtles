4876232015dd617117cfd90891c8a74e3d106972
RomanDeyres <roman.deyres@laposte.net>
2015-10-19 16:27:11 +0200

    ca marche les pedales

--- main.cpp
+++ main.cpp
@@ -15,14 +15,16 @@ using namespace std;
 
 const double g = 9.81;
 
-double *sansFrottement(double v0, double thetarad, double t)
+double *sansFrottement(double v0, double thetarad, double t, double x0)
 {
+    //cout << "fonction sans frottement" << endl;
     double x,y;
-    x = v0*cos(thetarad)*t/1000;
+    x = v0*cos(thetarad)*t/1000+x0;
     y = -g*t*t/(2*1000*1000)+v0*sin(thetarad)*t/1000;
     double *u = new double[2];
     u[0] = x;
     u[1] = y;
+    //cout << "coordonnées x:" << u[0] << " et y:" << u[1] << endl;
     return u;
 }
 
@@ -46,7 +48,11 @@ int main(int argc, char **argv)
     //  - tau : coefficient de frottement (rapport m/k)
     //  - xv = v0.cos(theta0)
     //  - yv = v0.sin(theta0)
-    double v0,theta0,thetarad,tau,xt,yt;
+    //  - x0 = déplacement du repère pour le rebond de la balle
+    double v0,theta0,thetarad,tau,xt,yt,x0=0;
+
+    // - Coefficient de rebondissement
+    double coeffRebond;
 
     //  Déclaration des variables temporelles
     double t;
@@ -55,7 +61,7 @@ int main(int argc, char **argv)
     dt = 100;       //  dt en ms
 
     //  Déclaration de la position
-    double x,y,y1;
+    double x1,y1;
 
     //  Déclaration des vitesses v1
     double v1,v1x,v1y,v1ynew;
@@ -82,7 +88,7 @@ int main(int argc, char **argv)
         int choix;
         cout << "Bienvenue dans le menu de flyingTurtles !!" << endl;
         cout << endl << "Choisissez votre mode !" << endl;
-        cout << "1) Lancer simple d'une tortue sans frottement du à l'air" << endl;
+        cout << "1) Lancer simple d'une tortue sans frottement du à l'air mais avec des rebonds" << endl;
         cout << "2) Lancer simple d'une tortue avec frottement du à l'air ou à un fluide" << endl;
         cin >> choix;
 
@@ -90,60 +96,37 @@ int main(int argc, char **argv)
         {
             cout << "Rentrez la vitesse initial (en m/s) de la boule !" << endl;
             cin >> v0;
-            cout << "A quelle angle voulez vous lancer la balle (angle en degré par rapport à        l'horizontal) ?" << endl;
+            cout << "A quelle angle voulez vous lancer la balle"<< endl <<"(angle en degré par rapport à l'horizontal donc entre 0 et 90°) ?" << endl;
             cin >> theta0;
+            cout << "Donner le coefficient de rebondissement du sol"<< endl <<"(entre 0 et 1; 1 rebond sans frottement) ?" << endl;
+            cin >> coeffRebond;
 
             thetarad = theta0*2*M_PI/360;
             xt = v0*cos(thetarad);
             yt = v0*sin(thetarad);
 
-            //  initialisation de root
-            //TApplication theApp("App", &argc, argv);
-
-            //  création de la fenêtre
-            //TCanvas c("c","fenetre",400,400);
-
-            //  coordonnées de la fenetre
-            //c.Range(0,0,300,300);
-
-            //  dessin de la balle à t=0
-            /*TEllipse balle(10,10,5);
-            balle.SetFillColor(kBlack);
-            balle.Draw();
-
-            //  dessin du vecteur v0
-            TArrow *l=new TArrow(10,10,xv,yv); // x1,y1,x2,y2
-            l->SetLineColor(kBlue);
-            l->Draw();*/
             double precedente[2];
 
-            while(1)
+            //cout << "on entre dans la boucle while" << endl;
+
+            while(x1<300)
             {
-                double *coord = sansFrottement(v0,thetarad,t);
-
-                if(y1<0)
+                if(y1 < 0)
                 {
-                    v0 = 0.9*sqrt((balle.GetX1()- precedente[0])*(balle.GetX1()- precedente[0])+(balle.GetY1()- precedente[1])*(balle.GetY1()- precedente[1]));
-
-                    //v1x = v0*cos(thetarad);
-                    //v1y = -v0*sin(thetarad);
-                    v0 = sqrt(v1x*v1x+v1y*v1y);
-
-                    coord = sansFrottement(v0,thetarad,t);
+                    v0 *= coeffRebond;
+                    t = 0;
+                    x0 = x1;
                 }
-
+                double *coord = sansFrottement(v0,thetarad,t,x0);
                 t += dt;
+                x1 = coord[0];
                 y1 = coord[1];
                 balle.SetX1(coord[0]);
                 balle.SetY1(coord[1]);
                 balle.Draw();
 
-
-                precedente[0] = coord[0];
-                precedente[1] = coord[1];
-
                 this_thread::sleep_for(chrono::milliseconds{dt/2});   // attend dt millisecondes
-                c.Update();                                         // mise à jour de la fenêtre c
+                c.Update();                                           // mise à jour de la fenêtre c
             }
 
             c.Closed();
@@ -153,7 +136,7 @@ int main(int argc, char **argv)
         {
             cout << "Rentrez la vitesse initial (en m/s) de la boule !" << endl;
             cin >> v0;
-            cout << "A quelle angle voulez vous lancer la balle (angle en degré par rapport à        l'horizontal) ?" << endl;
+            cout << "A quelle angle voulez vous lancer la balle (angle en degré par rapport à        l'horizontal donc entre 0 et 90°) ?" << endl;
             cin >> theta0;
             cout << "Donner le rapport m/k qui représente le coefficient de frottement ?" << endl;
             cin >> tau;
@@ -162,26 +145,7 @@ int main(int argc, char **argv)
             xt = v0*cos(thetarad);
             yt = v0*sin(thetarad);
 
-            //  initialisation de root
-            //  TApplication theApp("App", &argc, argv);
-
-            //  création de la fenêtre
-            /*TCanvas c("c","fenetre",400,400);
-
-            //  coordonnées de la fenetre
-            c.Range(0,0,300,300);
-
-            //  dessin de la balle à t=0
-            TEllipse balle(10,10,5);
-            balle.SetFillColor(kBlack);
-            balle.Draw();
-
-            //  dessin du vecteur v0
-            TArrow *l=new TArrow(10,10,xv,yv); // x1,y1,x2,y2
-            l->SetLineColor(kBlue);
-            l->Draw();*/
-
-            while(y1>0)
+            while(y1>=0)
             {
                 double *coord = avecFrottement(v0,thetarad,t,tau);
                 t += dt;
@@ -194,8 +158,7 @@ int main(int argc, char **argv)
                 c.Update();                                           // mise à jour de la fenêtre c
             }
 
-            c.Close();
-            //Close(TApplication *theApp);
+            c.Closed();
         }
 
     }
